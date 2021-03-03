a2f69f4b7e4684d98b43e699d1e95aea94dd94d4
Kalyani Kajawe <kajawek@gmail.com>
2021-03-03 18:55:12 +0100

    Feature: Add Initial project setup #1

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>React Native Components explaination in detail</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
