41ca9f6aa5e4d8e5929fd870ea6449d1a43ec2d7
Kalyani Kajawe <kajawek@gmail.com>
2021-03-09 18:30:27 +0100

    Feature: Adding icons, adding images locally, n/w images, scrollview, FlatList,Button-styling for Icons - use package - @expo/vector-icons

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Image, View, Text} from 'react-native';
+import CustomBoldText from '../components/CustomBoldText';
+import CustomText from '../components/CustomText';
+import MainButton from '../components/MainButton';
+import colors from '../constants/colors';
 
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>The Game is Over!!</Text>
-      <Text>Number of Rounds : {props.numOfRounds} </Text>
-      <Text>Number was : {props.guessingNumber} </Text>
-      <View><Button title="New Game" onPress={props.onRestart}/></View>
+      <CustomBoldText style={styles.titleText}>
+        The Game is Over!!
+      </CustomBoldText>
+      <View style={styles.imageContainer}>
+        <Image
+        fadeDuration= {1000}
+          style={styles.image}
+          // source={require('../assets/images/success.png')}
+          source={{uri: 'https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_960_720.jpg'}}
+          resizeMode="cover"
+        />
+      </View>
+      <View style={styles.resultContainer}>
+      <CustomText style={styles.bodyText}>
+        Your phone needed <Text style={styles.highlightText}>{props.numOfRounds}</Text> rounds to guess the number <Text style={styles.highlightText}>{props.guessingNumber}</Text>
+      </CustomText>
+      </View>
+      <View>
+        <MainButton onPress={props.onRestart}>New Game</MainButton>
+      </View>
     </View>
   );
 };
@@ -17,5 +37,35 @@ const styles = StyleSheet.create({
     padding: 10,
     alignItems: 'center',
   },
+  titleText: {
+    fontSize: 20,
+  },
+  bodyText: {
+    fontSize: 18,
+    textAlign: 'center'
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  imageContainer: {
+    borderRadius: 150,
+    borderWidth:3 ,
+    borderColor:'black',
+    width: 300,
+    height: 300,
+    overflow: 'hidden',
+    marginVertical:30
+
+
+  },
+  resultContainer:{
+    marginHorizontal: 30,
+    marginVertical: 15
+  },
+  highlightText:{
+      color: colors.primary,
+      fontFamily: 'OpenSans-Bold'
+  }
 });
 export default GameOverScreen;
